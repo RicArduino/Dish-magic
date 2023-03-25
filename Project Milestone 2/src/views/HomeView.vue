d6c581302534afb023bde0de599dceaa7df7f492
ilam94 <ilam.petetin.kaddachi@gmail.com>
2023-03-24 20:22:37 -0400

    j'suis la boss

--- Project Milestone 2/src/views/HomeView.vue
+++ Project Milestone 2/src/views/HomeView.vue
@@ -9,15 +9,12 @@ import Footer from "@/components/FooterTest.vue";
 import deco1 from "../assets/deco1.png";
 import deco2 from "../assets/deco2.png";
 import deco3 from "../assets/deco3.png";
-import deco4 from "../assets/deco4.png";
-import deco5 from "../assets/deco5.png";
-import deco6 from "../assets/deco6.png";
-import deco7 from "../assets/deco7.png";
-import deco8 from "../assets/deco8.png";
-import deco9 from "../assets/deco9.png";
-import deco10 from "../assets/deco10.png";
-import deco11 from "../assets/deco11.png";
 import dish1 from "../assets/dish1.png";
+import dish2 from "../assets/dish2.png";
+import dish3 from "../assets/dish3.png";
+import dish4 from "../assets/dish4.png";
+
+
 export default function HomeView() {
   return (
     <>
@@ -28,21 +25,75 @@ export default function HomeView() {
           <img class="deco-2" src={deco2} alt="deco2" />
           <img class="deco-3" src={deco3} alt="deco3" />
           <div class="container-1-1">
-            <h1 class="container-1-1-title">Discover recipe about all type of culture</h1>
-            <p class ="container-1-1-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h1 class="container-1-1-title">
+              Discover recipe about all type of culture
+            </h1>
+            <p class="container-1-1-subtitle">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
             <button class="container-1-1-button-1">Catalog</button>
             <button class="container-1-1-button-2">Random recipe</button>
             <img class="container-1-1-dish-1" src={dish1} alt="dish1" />
           </div>
         </div>
+        <div class="container-2">
+          <div class="container-2-1">
+            <h1>Favorite Dishes</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          <div class="container-2-2">
+            <div class="products">
+              <div class="solo_product">
+                <div class="solo_img">
+                  <img class="solo_img1" src={dish2} alt="Dish Image" />
+                </div>
+                <div class="solo_text">
+                  <div class="card_title">
+                    <p class="product_name">Lumpia with Sauce</p>
+                    <p class="product_rate">44 K</p>
+                  </div>
+                  <div class="card_text">
+                    <p class="product_description">Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor</p>
+                  </div>
+                </div>
+              </div>
+              <div class="solo_product">
+                <div class="solo_img">
+                  <img class="solo_img1" src={dish3} alt="Dish Image" />
+                </div>
+                <div class="solo_text">
+                  <div class="card_title">
+                    <p class="product_name">Tofu Chili</p>
+                    <p class="product_rate">21 K</p>
+                  </div>
+                  <div class="card_text">
+                    <p class="product_description">Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor</p>
+                  </div>
+                </div>
+              </div>
+              <div class="solo_product">
+                <div class="solo_img">
+                  <img class="solo_img1" src={dish4} alt="Dish Image" />
+                </div>
+                <div class="solo_text">
+                  <div class="card_title">
+                    <p class="product_name">Egg and cucumber</p>
+                    <p class="product_rate">36 K</p>
+                  </div>
+                  <div class="card_text">
+                    <p class="product_description">Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="container-2">
-        <img class="container-2-deco-1" src={deco10} alt="deco1" />
-        <img class="container-2-deco-2" src={deco11} alt="deco2" />
-        <img class="container-2-deco-3" src={deco4} alt="deco3" />
-        <img class="container-2-deco-4" src={deco5} alt="deco4" />
-      </div>
+
       <Footer />
     </>
   );
@@ -54,6 +105,7 @@ body {
   height: 100%;
   margin: 0;
 }
+
 .deco-1 {
   position: absolute;
   top: 50px;
@@ -95,100 +147,180 @@ body {
   left: 156px;
   top: 200px;
 }
-.container-1-1-title{
+.container-1-1-title {
   position: absolute;
-width: 674px;
-height: 216px;
-left: 16px;
-top: 24px;
+  width: 674px;
+  height: 216px;
+  left: 16px;
+  top: 24px;
 
-font-family: 'Inter';
-font-style: normal;
-font-weight: 700;
-font-size: 70px;
-line-height: 72px;
-text-align: left;
-/* or 103% */
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 70px;
+  line-height: 72px;
+  text-align: left;
+  /* or 103% */
 
-
-color: #101A24;
+  color: #101a24;
 }
 
-.container-1-1-subtitle{
+.container-1-1-subtitle {
   position: absolute;
-width: 376px;
-height: 115px;
-left: 24px;
-top: 280px;
+  width: 376px;
+  height: 115px;
+  left: 24px;
+  top: 280px;
 
-font-family: 'Inter';
-font-style: normal;
-font-weight: 400;
-font-size: 16px;
-line-height: 29px;
-text-align: left;
-/* or 181% */
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 29px;
+  text-align: left;
+  /* or 181% */
 
-
-color: #5C6574;
+  color: #5c6574;
 }
 
-.container-1-1-button-1{
+.container-1-1-button-1 {
   position: absolute;
-width: 120px;
-height: 54px;
-left: 24px;
-top: 400px;
-background-color: #101A24;
+  width: 120px;
+  height: 54px;
+  left: 24px;
+  top: 400px;
+  background-color: #101a24;
 }
 
-.container-1-1-button-2{
+.container-1-1-button-2 {
   position: absolute;
-width: 167px;
-height: 54px;
-left: 167px;
-top: 400px;
+  width: 167px;
+  height: 54px;
+  left: 167px;
+  top: 400px;
 }
 
-.container-1-1-dish-1{
+.container-1-1-dish-1 {
   position: absolute;
-width: 364px;
-height: 364px;
-left: 552px;
-top: 208px;
+  width: 364px;
+  height: 364px;
+  left: 552px;
+  top: 208px;
 }
 .container-2 {
-  top: 50%;
-  height: 1874px;
+  top: 1000px;
+  height: 1000px;
   background: #101a2422;
+  font-family: "Inter";
 }
 
-.container-2-deco-1 {
-  position: absolute;
-left: 18.6%;
-right: 66.29%;
-top: 120.24%;
-
+.container-2-1 {
+  position: relative;
+  margin: auto auto;
+  width: 50%;
+  top: 100px;
+}
+.container-2-2 {
+  position: relative;
+  margin: auto auto;
+  width: 50%;
+  top: 100px;
 }
 
-.container-2-deco-2 {
-  position: absolute;
-left: 62.4%;
-right: 25.43%;
-top:  120.57%;
+.products {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+  max-width: 1200px;
 }
 
-.container-2-deco-3 {
-  position: absolute;
-left: 81.04%;
-right: 10.97%;
-top: 170.57%
+.solo_product {
+  border: #5c5c5c39 6px solid;
+  margin: 20px;
+  border-radius: 10px;
+  background-color: #ffffff;
+  max-width: 300px;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 10px;
 }
 
-.container-2-deco-4 {
-  position: absolute;
-left: 73.18%;
-right: 20.73%;
-top: 170.62%;
-  }
+.solo_product:hover {
+  border: #ffffff 6px solid;
+}
+
+.solo_img1 {
+  max-width: 150px;
+  max-height: 100px;
+}
+
+.product_name {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 27px;
+  /* identical to box height */
+
+  display: flex;
+  align-items: center;
+  text-align: left;
+
+  color: #000000;
+}
+
+.product_rate {
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 27px;
+  /* identical to box height */
+
+  display: flex;
+  align-items: center;
+  text-align: right;
+
+  color: #000000;
+}
+
+.card_title {
+  font-family: "Inter", sans-serif;
+  display: flex;
+  justify-content: space-between;
+  font-weight: bold;
+  font-size: 20px;
+  padding-right: 10px;
+  padding-left: 10px;
+}
+
+.cart {
+  text-align: right;
+  padding-top: 20px;
+  margin-right: 5px;
+  font-size: 10px;
+}
+.card_text{
+  font-family: "Inter";
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  max-width: 200px;
+}
+.product_description{
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+  /* or 181% */
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #5C6574;
+}
 </style>
